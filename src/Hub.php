af66ac79985a66169c1db5a23915ee19f570f139
Tom Wright <tom@inflatablecookie.com>
2022-11-22 17:30:22 +0000

    Fixed error handling
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Hub.php
+++ src/Hub.php
@@ -14,12 +14,16 @@ use DecodeLabs\Clip\Controller as ControllerInterface;
 use DecodeLabs\Clip\Hub as ClipHub;
 use DecodeLabs\Clip\Task as TaskInterface;
 use DecodeLabs\Effigy;
+use DecodeLabs\Glitch;
+use DecodeLabs\Terminus;
 use DecodeLabs\Veneer;
 
 class Hub extends ClipHub
 {
     public function initializePlatform(): void
     {
+        parent::initializePlatform();
+
         Archetype::extend(TaskInterface::class, Task::class);
 
         $controller = new Controller(
@@ -30,6 +34,17 @@ class Hub extends ClipHub
 
         $this->context->container->bindShared(ControllerInterface::class, $controller);
         $this->context->container->bindShared(Controller::class, $controller);
+
+
+        set_exception_handler(function ($e) use ($controller) {
+            if ($controller->isLocal()) {
+                Glitch::handleException($e);
+            } else {
+                Terminus::newLine();
+                Terminus::error($e->getMessage());
+                Terminus::newLine();
+            }
+        });
 
         /** @phpstan-ignore-next-line */
         Veneer::register(Controller::class, Effigy::class);
