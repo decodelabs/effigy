30eba2b1570b3d4676f82a4a553dbcd467b2a0e5
Tom Wright <tom@inflatablecookie.com>
2022-11-24 17:34:11 +0000

    Added version task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Task/Version.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package Effigy
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Effigy\Task;
+
+use DecodeLabs\Clip\Task;
+use DecodeLabs\Integra;
+use DecodeLabs\Terminus as Cli;
+
+class Version implements Task
+{
+    public function execute(): bool
+    {
+        return Integra::runGlobal('show', 'decodelabs/effigy');
+    }
+}
