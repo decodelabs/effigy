a12cac744bfb1be5ac94dfb122cd39af1b25e061
Tom Wright <tom@inflatablecookie.com>
2022-11-25 23:34:55 +0000

    Reset entry task decoration
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Controller.php
+++ src/Controller.php
@@ -157,7 +157,6 @@ class Controller extends GenericController implements
             // Launch script
             return $this->newScriptLauncher($entry->getPath(), [$name, ...$args])
                 ->addSignal('SIGINT', 'SIGTERM', 'SIGQUIT')
-                ->setDecoratable(false)
                 ->launch()
                 ->wasSuccessful();
         }
