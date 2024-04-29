681c145d57a77c4122c5f61c04861defc2726a69
Tom Wright <tom@inflatablecookie.com>
2024-04-29 08:48:58 +0000

    Updated ECS config
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- ecs.php
+++ ecs.php
@@ -7,7 +7,9 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
-    ->withPaths([__DIR__ . '/src'])
+    ->withPaths([
+        __DIR__ . '/src'
+    ])
     ->withPreparedSets(
         cleanCode: true,
         psr12: true
