ee5f8b38b364a33fce9eb832aa82c1dca4a5cb33
Tom Wright <tom@inflatablecookie.com>
2025-08-21 22:24:34 +0000

    Fixed Clip controller mapping
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Effigy/Hub.php
+++ src/Effigy/Hub.php
@@ -9,14 +9,18 @@ declare(strict_types=1);
 
 namespace DecodeLabs\Effigy;
 
+use DecodeLabs\Clip;
 use DecodeLabs\Clip\Hub as ClipHub;
 use DecodeLabs\Commandment\Action as ActionInterface;
+use DecodeLabs\Effigy;
 
 class Hub extends ClipHub
 {
     public function initializePlatform(): void
     {
         parent::initializePlatform();
+
+        $this->container->setType(Clip::class, Effigy::class);
 
         // @phpstan-ignore-next-line
         $this->archetype->map(ActionInterface::class, Action::class);
